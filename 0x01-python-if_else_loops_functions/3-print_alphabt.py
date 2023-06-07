d953a8b28adf236849ece0d52979b7ddce9a7c97
Isaac Boakye <isaacboakye77@outlook.com>
2023-06-06 22:34:36 -0700

    Task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for a in range(97, 123):
-    print("{:c}".format(a), end= "") if (a != 113 and a != 101) else "" 
+    print("{:c}".format(a), end="") if (a != 113 and a != 101) else ""
