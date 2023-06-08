bd230d87942b036f9826daf92b59b46623e7463a
Isaac Boakye <isaacboakye77@outlook.com>
2023-06-07 22:12:47 -0700

    Task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for a in range(1, 101):
-        if a % 5 == 0:
+        if (a % 5 == 0) and (a % 3 == 0):
+            print("FizzBuzz", end=" ")
+        elif a % 5 == 0:
             print("Buzz", end=" ")
         elif a % 3 == 0:
             print("Fizz", end=" ")
