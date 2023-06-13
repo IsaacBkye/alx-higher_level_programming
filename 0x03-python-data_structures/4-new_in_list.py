9a82325d8ae49efbb2ec2be2af5ff062e3ccb24c
Isaac Boakye <isaacboakye77@outlook.com>
2023-06-12 17:20:07 -0700

    Task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+    newList = list(my_list)
+    if (idx >= 0 or idx < size):
+        for i, cnt in enumerate(newList):
+            if (i == idx):
+                newList[i] = element
+    return (newList)
