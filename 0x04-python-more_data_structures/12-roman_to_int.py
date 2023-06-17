8f0e212659c493c190e637e08ea9592496e2533f
Isaac Boakye <isaacboakye77@outlook.com>
2023-06-16 21:50:18 -0700

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    digit = 0
+    if (roman_string.__class__ == str):
+        rString = len(roman_string)
+        rDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        rValue = [rDict.get(_value) for _value in roman_string]
+        for item, _value in enumerate(rValue):
+            if (item < rString - 1):
+                if (_value < rValue[item + 1]):
+                    digit -= _value
+                else:
+                    digit += _value
+            else:
+                digit += _value
+    return (digit)
