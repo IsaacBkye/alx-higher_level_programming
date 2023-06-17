d984bdb6e321a59e7639d761103d38275f92de9d
Isaac Boakye <isaacboakye77@outlook.com>
2023-06-16 21:53:01 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     digit = 0
     if (roman_string.__class__ == str):
         rString = len(roman_string)
-        rDict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        rValue = [rDict.get(_value) for _value in roman_string]
+        rD = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        rValue = [rD.get(_value) for _value in roman_string]
         for item, _value in enumerate(rValue):
             if (item < rString - 1):
                 if (_value < rValue[item + 1]):
