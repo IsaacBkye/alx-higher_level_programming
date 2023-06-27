c76a8182db2278f39e4be6a0adcf8830e5a578b2
Isaac Boakye <isaacboakye77@outlook.com>
2023-06-26 18:54:40 -0700

    Task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    _print = 0
+    try:
+        for a in range(0, x):
+            print(my_list[i], end="")
+            _print += 1
+    except IndexError:
+        None
+    print()
+    return (_print)
