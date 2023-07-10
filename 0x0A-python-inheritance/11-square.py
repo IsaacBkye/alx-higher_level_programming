3a79b5b952e8766ddd1f9898a479154b53e874c5
Isaac Boakye <isaacboakye77@outlook.com>
2023-07-10 16:53:43 -0700

    Task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""Module for Basic rectangle calc"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class inherited from Rectangle.
+    """
+
+    def __init__(self, size):
+        """
+        Constructor/initializer method
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Returns Square description
+        """
+        return ("[Square] {}/{}".format(self.__size, self.__size))
