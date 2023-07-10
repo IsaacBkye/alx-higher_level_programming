81b7a2d640d0fa1d0ca5f42ca16028ab3faf16fe
Isaac Boakye <isaacboakye77@outlook.com>
2023-07-10 16:42:55 -0700

    Task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+""" Module for basic geometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class inheirted from BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+        """
+        Constructor method
+        """
+        self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.__width = width
+        self.__height = height
