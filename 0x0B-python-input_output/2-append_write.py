40cf5f6edd08e484330e2ae75e96d3f7258c60c2
Isaac Boakye <isaacboakye77@outlook.com>
2023-07-12 03:53:09 -0700

    Task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""Module for appending to a file"""
+
+
+def append_write(filename="", text=""):
+    """
+    Writes a string to a text file, also returns the number of characters written
+    """
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
