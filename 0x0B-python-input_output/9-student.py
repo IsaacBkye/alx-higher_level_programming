5572e83eafebe0b6af40d2685d4a2078c3b82bb6
Isaac Boakye <isaacboakye77@outlook.com>
2023-07-12 05:05:11 -0700

    Task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+
+"""Module for Student class"""
+
+
+class Student:
+    """
+    Student class definition
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializer/Constructor method
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Dictionary of a Student instance with attrs validation
+        """
+        if (type(attrs) is list):
+            newDict = {}
+            Dict = vars(self)
+            setDict = set(Dict)
+            setList = set(attrs)
+            keys = list(setDict & setList)
+            for key in keys:
+                newDict[key] = Dict[key]
+            return (newDict)
+        return vars(self)
