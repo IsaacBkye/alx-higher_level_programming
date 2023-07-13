ff6b1bf9844a06a1f8e791e1b80851251d7a44b3
Isaac Boakye <isaacboakye77@outlook.com>
2023-07-12 17:58:35 -0700

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""Module for Pascal Traingle"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    myList = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for elem in range(1, i):
+            row.append(myList[i - 1][elem - 1] + myList[i - 1][elem])
+        row.append(1)
+        myList.append(row)
+    return myList
