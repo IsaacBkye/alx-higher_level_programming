3eb2a626244ac94809c89285a35cc025fd7ba5a1
Isaac Boakye <isaacboakye77@outlook.com>
2023-09-05 19:03:03 -0700

    Task 17

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,7 @@
 -- script to display max temp of state
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state;
+
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state ASC
+LIMIT 3;
