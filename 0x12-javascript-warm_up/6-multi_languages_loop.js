1e7779c07382a14c8f5971bd9355cb2a2526cdca
IsaacBkye <isaacboakye77@outlook.com>
2023-09-11 17:08:26 -0700

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// To print 3 lines: by using an array of string and a loop
+
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; i < 3; i++) {
+  console.log(langs[i]);
+}
